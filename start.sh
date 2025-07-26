01177b155f40e61d61a4fca2b96244022f561d17
fr10pro <realemonfx@gmail.com>
2025-07-26 17:17:06 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# (Optional debug) Show installed packages:
+# (Optional) Debug installed packages:
 # ./.venv/bin/pip list
 
-# Run uvicorn via the venv’s Python
+# Launch via the venv’s Python interpreter
 ./.venv/bin/python -m uvicorn main:app --host 0.0.0.0 --port $PORT
